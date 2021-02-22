d68ab86b9d2adf35cf02d32f076be56c407517bf
LeesinDong <32248827@qq.com>
2021-02-22 13:50:35 +0800

    fix

--- note/gupao/15 有序树/01 提示词  [208]/Trie.java
+++ note/gupao/15 有序树/01 提示词  [208]/Trie.java
@@ -1,3 +1,106 @@
+// *****************************************************写这个********************************************************
+class TrieNode {
+    char c;
+    Map<Character, TrieNode> children = new HashMap<>();
+    boolean hashWord;
+    public TrieNode() {
+
+    }
+
+    public TrieNode(char c) {
+        this.c = c;
+    }
+}
+class Trie {
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        this.root = new TrieNode();
+    }
+
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode current = root;
+        Map<Character, TrieNode> children = current.children;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            if (!children.containsKey(arr[i])) {
+                TrieNode node = new TrieNode(arr[i]);
+                current = node;
+                children.put(arr[i], node);
+            } else {
+                TrieNode node = children.get(arr[i]);
+                current = node;
+            }
+            children = current.children;
+            if (i == arr.length - 1) {
+                current.hashWord = true;
+            }
+        }
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode current = root;
+        Map<Character, TrieNode> children = current.children;
+        char[] arr = word.toCharArray();
+        for (int i = 0; i < word.length(); i++) {
+            if (children.containsKey(arr[i])) {
+                TrieNode node = children.get(arr[i]);
+                current = node;
+                children = current.children;
+            } else {
+                return false;
+            }
+
+        }
+        if (current.hashWord == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+        Map<Character, TrieNode> children = current.children;
+        char[] arr = prefix.toCharArray();
+        for (int i = 0; i < prefix.length(); i++) {
+            if (children.containsKey(arr[i])) {
+                TrieNode node = children.get(arr[i]);
+                current = node;
+                children = current.children;
+            } else {
+                return false;
+            }
+
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
+
+
+
+
+
+
+// *****************************************************原来的版本********************************************************
+
+
+
+
+
+
+
 // 本质：移动current、children + 一开始map.put
 class TrieNode {
     // 三样东西：1 当前node的字符串 2 children 3 是否找到了word
@@ -26,6 +129,7 @@ public class Trie {
     
     /** Inserts a word into the trie. */
     public void insert(String word) {
+        // *********************注意是root
         TrieNode current = root;
         HashMap<Character, TrieNode> children = current.children;
         char[] wordArray = word.toCharArray();
@@ -55,27 +159,27 @@ public class Trie {
     
     /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        HashMap<Character, TrieNode> children = root.children;
-        TrieNode current = null;
-        char[] wordArray = word.toCharArray();
-
-        for (int i = 0; i < wordArray.length; i++) {
-            // 已经有了，移动current、移动children
-            if (children.containsKey(wordArray[i])) {
-                current = children.get(wordArray[i]);
-                children = current.children;
+            HashMap<Character, TrieNode> children = root.children;
+            TrieNode current = null;
+            char[] wordArray = word.toCharArray();
+    
+            for (int i = 0; i < wordArray.length; i++) {
+                // 已经有了，移动current、移动children
+                if (children.containsKey(wordArray[i])) {
+                    current = children.get(wordArray[i]);
+                    children = current.children;
+                } else {
+                    // 没有返回false
+                    return false;
+                }
+            }
+    
+            // current到了最后,即找到了，因为插入的时候最后一个hasWord是true
+            if (current.hasWord){
+                return true;    
             } else {
-                // 没有返回false
                 return false;
             }
-        }
-
-        // current到了最后,即找到了，因为插入的时候最后一个hasWord是true
-        if (current.hasWord){
-            return true;    
-        } else {
-            return false;
-        }
     }
     
     /** Returns if there is any word in the trie that starts with the given prefix. */
